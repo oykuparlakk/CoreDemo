068916d5ea8bc29702b135fe2458a8a1d08437ce
oyku <oyku@DESKTOP-73QGJ2T>
2022-04-02 04:24:52 -0700

    5.0.12

---
+++ DataAccessLayer/Abstract/IBlogDal.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IBlogDal
+    {
+        List<Blog> ListAllBlog();
+        void AddBlog(Blog blog);
+        void RemoveBlog(Blog blog);
+        void UpdateBlog(Blog blog);
+        Blog GetBlog(int id);
+    }
+}
---
+++ DataAccessLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context _context = new Context();
+        public void AddCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+        }
+
+        public Category GetCategory(int id)
+        {
+            return _context.Categories.Find(id);   
+        }
+
+        public List<Category> ListAllCategory()
+        {
+            return _context.Categories.ToList();
+        }
+
+        public void RemoveCategory(Category category)
+        {
+            _context.Remove(category);
+            _context.SaveChanges();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            _context.Update(category);
+        }
+    }
+}
